f7415914afd02b38a18396036b4db8cc20f1e71f
mqois16 <qoisoctava@gmail.com>
2021-05-04 15:12:17 +0700

    AccountFragment 50% firebase

--- app/src/main/java/android/example/walkee/Account.java
+++ app/src/main/java/android/example/walkee/Account.java
@@ -7,6 +7,12 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.SignInButton;
+import com.google.firebase.auth.FirebaseAuth;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -14,6 +20,11 @@ import android.view.ViewGroup;
  * create an instance of this fragment.
  */
 public class Account extends Fragment {
+    private SignInButton signInButton;
+    private GoogleSignInClient mGoogleSignInClient;
+    private String TAG = "fragment_account";
+    private FirebaseAuth mAuth;
+    private Button btnSignOut;
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -52,6 +63,16 @@ public class Account extends Fragment {
         if (getArguments() != null) {
             mParam1 = getArguments().getString(ARG_PARAM1);
             mParam2 = getArguments().getString(ARG_PARAM2);
+
+            signInButton = getView().findViewById(R.id.signin_btn);
+            mAuth = FirebaseAuth.getInstance();
+            btnSignOut = getView().findViewById(R.id.signout_btn);
+
+            GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                    .requestIdToken(getString(R.string.default_web_client_id))
+                    .requestEmail()
+                    .build()
+
         }
     }
 
