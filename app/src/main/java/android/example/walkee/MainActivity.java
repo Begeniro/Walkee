7e391a3a345603ee7fe333e77741bd789cf58604
Begeniro <61041689+Begeniro@users.noreply.github.com>
2021-05-09 02:36:16 +0700

    Revisi FrontEnd Step Counter Work

--- app/src/main/java/android/example/walkee/MainActivity.java
+++ app/src/main/java/android/example/walkee/MainActivity.java
@@ -6,7 +6,13 @@ import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentTransaction;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.graphics.drawable.AnimationDrawable;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.os.Bundle;
 import android.view.MenuItem;
 import android.widget.FrameLayout;
@@ -16,54 +22,24 @@ import com.google.android.material.bottomnavigation.BottomNavigationMenuView;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
-
-    private BottomNavigationView mMainNav;
-    private FrameLayout mMainFrame;
-
+    //background animation
     private ConstraintLayout constraintLayout;
     private AnimationDrawable animationDrawable;
 
-    private TrackerFragment trackerFragment;
-    private Account accountFragment;
+    //bottom navigation
+    private BottomNavigationView mMainNav;
 
-    TextView header;
+    //step counter
+    private TextView countView;
+    private double MagnitudePrevious = 0;
+    private Integer stepCount = 0;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        header=findViewById(R.id.header);
 
-        mMainFrame=findViewById(R.id.mainFrame);
-        mMainNav=findViewById(R.id.bottNav);
-
-        trackerFragment = new TrackerFragment();
-        accountFragment = new Account();
-        setFragment(trackerFragment);
-        header.setText("Step Tracker");
-
-        //BottomBar
-        mMainNav.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
-            @Override
-            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-                switch (item.getItemId()) {
-                    case R.id.nav_tracker :
-                        setFragment(trackerFragment);
-                        header.setText("Step Tracker");
-                        return true;
-                    case R.id.nav_history :
-                        setFragment(trackerFragment);
-                        header.setText("History");
-                        return true;
-                    case R.id.nav_account :
-                        setFragment(accountFragment);
-                        header.setText("Account");
-                        return true;
-                    default :
-                        return false;
-                }
-            }
-        });
-
+        //no action bar
         getSupportActionBar().hide();
 
         // init constraintLayout
@@ -78,11 +54,66 @@ public class MainActivity extends AppCompatActivity {
         // setting exit fade animation duration to 2 seconds
         animationDrawable.setExitFadeDuration(1500);
 
-        mMainFrame=findViewById(R.id.mainFrame);
+        //init bottom navigation
         mMainNav=findViewById(R.id.bottNav);
 
-        trackerFragment = new TrackerFragment();
-        accountFragment = new Account();
+        //set activity selected
+        mMainNav.setSelectedItemId(R.id.nav_tracker);
+
+        //BottomBar
+        mMainNav.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()) {
+                    case R.id.nav_tracker :
+                        return true;
+
+                    case R.id.nav_history :
+                        startActivity(new Intent(getApplicationContext()
+                        ,Riwayat.class));
+                        overridePendingTransition(0,0);
+                        return true;
+
+                    case R.id.nav_account :
+                        startActivity(new Intent(getApplicationContext()
+                                ,Akun.class));
+                        overridePendingTransition(0,0);
+                        return true;
+                }
+                return false;
+            }
+        });
+
+        //step counter
+        countView = findViewById(R.id.textViewCounter);
+        SensorManager sensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
+        Sensor sensor = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+
+        SensorEventListener stepDetector = new SensorEventListener() {
+            @Override
+            public void onSensorChanged(SensorEvent sensorEvent) {
+                if (sensorEvent!= null){
+                    float x_acceleration = sensorEvent.values[0];
+                    float y_acceleration = sensorEvent.values[1];
+                    float z_acceleration = sensorEvent.values[2];
+
+                    double Magnitude = Math.sqrt(x_acceleration*x_acceleration + y_acceleration*y_acceleration + z_acceleration*z_acceleration);
+                    double MagnitudeDelta = Magnitude - MagnitudePrevious;
+                    MagnitudePrevious = Magnitude;
+
+                    if (MagnitudeDelta > 6){
+                        stepCount++;
+                    }
+                    countView.setText(stepCount.toString());
+                }
+            }
+
+            @Override
+            public void onAccuracyChanged(Sensor sensor, int i) {
+            }
+        };
+
+        sensorManager.registerListener(stepDetector, sensor, SensorManager.SENSOR_DELAY_NORMAL);
 
     }
 
@@ -94,6 +125,9 @@ public class MainActivity extends AppCompatActivity {
             animationDrawable.start();
         }
 
+        SharedPreferences sharedPreferences = getPreferences(MODE_PRIVATE);
+        stepCount = sharedPreferences.getInt("stepCount", 0);
+
     }
 
     @Override
@@ -103,13 +137,22 @@ public class MainActivity extends AppCompatActivity {
             // stop the animation
             animationDrawable.stop();
         }
+
+        SharedPreferences sharedPreferences = getPreferences(MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        editor.clear();
+        editor.putInt("stepCount", stepCount);
+        editor.apply();
     }
 
-    private void setFragment(Fragment fragment) {
-        FragmentTransaction fragmentTransaction=getSupportFragmentManager().beginTransaction();
-        fragmentTransaction.replace(R.id.mainFrame, fragment);
-        fragmentTransaction.commit();
+    protected void onStop() {
+        super.onStop();
 
+        SharedPreferences sharedPreferences = getPreferences(MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        editor.clear();
+        editor.putInt("stepCount", stepCount);
+        editor.apply();
     }
 
 }
