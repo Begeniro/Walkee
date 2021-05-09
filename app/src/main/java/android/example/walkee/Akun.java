046253ba257623657f65604cd5a10c2b06f5b2e2
Begeniro <61041689+Begeniro@users.noreply.github.com>
2021-05-09 07:15:17 +0700

    Revisi FrontEnd Step Counter Work Push

---
+++ app/src/main/java/android/example/walkee/Akun.java
@@ -0,0 +1,93 @@
+package android.example.walkee;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
+
+import android.content.Intent;
+import android.graphics.drawable.AnimationDrawable;
+import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class Akun extends AppCompatActivity {
+    //background animation
+    private ConstraintLayout constraintLayout;
+    private AnimationDrawable animationDrawable;
+
+    //bottom navigation
+    private BottomNavigationView mMainNav;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_akun);
+
+        //no action bar
+        getSupportActionBar().hide();
+
+        // init constraintLayout
+        constraintLayout = (ConstraintLayout) findViewById(R.id.gradientbg);
+
+        // initializing animation drawable by getting background from constraint layout
+        animationDrawable = (AnimationDrawable) constraintLayout.getBackground();
+
+        // setting enter fade animation duration to 5 seconds
+        animationDrawable.setEnterFadeDuration(2000);
+
+        // setting exit fade animation duration to 2 seconds
+        animationDrawable.setExitFadeDuration(1500);
+
+        //init bottom navigation
+        mMainNav=findViewById(R.id.bottNav);
+
+        //set activity selected
+        mMainNav.setSelectedItemId(R.id.nav_account);
+
+        //BottomBar
+        mMainNav.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()) {
+                    case R.id.nav_tracker :
+                        startActivity(new Intent(getApplicationContext()
+                            ,MainActivity.class));
+                        overridePendingTransition(0,0);
+                        return true;
+
+                    case R.id.nav_history :
+                        startActivity(new Intent(getApplicationContext()
+                                ,Riwayat.class));
+                        overridePendingTransition(0,0);
+                        return true;
+
+                    case R.id.nav_account :
+
+                        return true;
+                }
+                return false;
+            }
+        });
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if (animationDrawable != null && !animationDrawable.isRunning()) {
+            // start the animation
+            animationDrawable.start();
+        }
+
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        if (animationDrawable != null && animationDrawable.isRunning()) {
+            // stop the animation
+            animationDrawable.stop();
+        }
+    }
+}
